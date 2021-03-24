28654ed48910c85c917607f16421a69ad204a926
Haruki Kondo <44923695+mashharuki@users.noreply.github.com>
2021-03-24 11:37:12 +0900

    Update createAddress.js

--- createAddress.js
+++ createAddress.js
@@ -1,8 +1,13 @@
+// bitcoin-libライブラリを読み込む 
 const bitcore = require('bitcore-lib');
+// テストネットを指定する。(本番はメインネット)
 const network = 'testnet';
+// const network = 'mainnet'
 
+// 秘密鍵生成
 const privateKey = new bitcore.PrivateKey(network);
+// 秘密鍵からアドレスを生成
 const address = privateKey.toAddress();
-
+// コンソール上に出力する。
 console.log(privateKey.toString());
 console.log(address.toString());
