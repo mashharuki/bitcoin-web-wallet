cc105fa551d2559c5f4fe81e2d011992f3e3f7aa
haruki kondo <avp.104.106.107@ae.auone-net.jp>
2021-07-23 21:07:51 +0900

    動くようにAPIのURLやhttpリクエストを実行するメソッドを修正しました。

--- test/test_multiSig.js
+++ test/test_multiSig.js
@@ -11,7 +11,7 @@ var insight = new explorers.Insight('testnet');
 
 /**
  * トランザクションをブロードキャストする関数
- * @param {}} tx 
+ * @param {} tx
  * @param {*} done 
  */
 function broadcast(tx, done){
@@ -37,12 +37,12 @@ function getUTXO(address, done) {
             console.log("insight utxo err",err);
             process.exit(1);
         }
-        //utxos.forEach(function(utxo){console.log(utxo.toJSON());});
+        // utxos.forEach(function(utxo){console.log(utxo.toJSON());});
         done(utxos[0]);
     });
  }
  
-// testnet
+// testnet用の秘密鍵を取得する
 var privateKey = bitcore.PrivateKey({
    "bn":"8c026a359a13f707a3497ef58da45b628958ff98b5f33322cf29ede12fcfd56f",
    "compressed":true,
@@ -55,53 +55,58 @@ var address = privateKey.toAddress();
  * テストコード
  */
 describe('challenge', function(){
- 
-   it('should have an address', function(){
-     expect(address.toString()).to.be.equal('myvsPNW9SgpmcGhBrYYowc5Q2cVHKqmuBP');
-   });
+
+    // アドレスをチエックするテスト
+    it('should have an address', function(){
+        expect(address.toString()).to.be.equal('myvsPNW9SgpmcGhBrYYowc5Q2cVHKqmuBP');
+    });
+
+    // マルチシグを利用したトランザクションをブロードキャストするテスト
+    it('should perform a testnet multisig transaction and refund', function(done){
+        // UTXOを取得する。
+        getUTXO(address, function(utxo){
+            console.log('UTXO:',utxo);
    
-   it('should perform a testnet multisig transaction and refund', function(done){
-     getUTXO(address, function(utxo){
-       console.log(utxo.toJSON());
-   
-       // test sending a small bit to a p2sh multisig
-       // 秘密鍵2つ
-       var privateKey1 = new bitcore.PrivateKey('612b3ca3f368cf2658c2e1777d2fa28e6bcde8ea19312cbf69e09e7333e13994', 'testnet');
-       var privateKey2 = new bitcore.PrivateKey('d65788b9947b41625ffff946bc145187c6b85d1686e60becdf34567f17478730', 'testnet');
-       // 公開鍵2つ
-       var publicKey1 = privateKey1.publicKey;
-       var publicKey2 = privateKey2.publicKey;
-       // スクリプトシグ生成
-       var P2SHScript = new bitcore.Script.buildMultisigOut([publicKey1, publicKey2], 1);
-       // スクリプトシグのハッシュ値を生成
-       var P2SHFund = P2SHScript.toScriptHashOut();
-       // トランザクション生成 
-       var tx = new bitcore.Transaction()
-         .from(utxo)
-         .to(P2SHFund.toAddress(), 9000)
-         .change(address)
-         .sign(privateKey); 
- 
-       console.log("tx",tx);
-       // ブロードキャストする。
-       broadcast(tx, function(id){
-         console.log("funded to",id);
- 　　　　　// 2つ目のトランザクション生成
-         var tx2 = new bitcore.Transaction()
-           .from({txId:id, outputIndex:0, inputIndex:0, satoshis:9000, script:P2SHFund.toString()}, [publicKey1, publicKey2], 1)
-           .to(address, 8000)
-           .sign(privateKey2); 
- 
-         console.log("tx2",tx2.serialize());
-         // ブロードキャストする。
-         broadcast(tx2, function(id2){
-           console.log("funded back to",id2);
-           expect(id2).to.be.a("string");
-           done()
-         });
-     
-       });
-   
-     });
-   })
+           // test sending a small bit to a p2sh multisig
+           // 秘密鍵2つ
+           var privateKey1 = new bitcore.PrivateKey('612b3ca3f368cf2658c2e1777d2fa28e6bcde8ea19312cbf69e09e7333e13994', 'testnet');
+           var privateKey2 = new bitcore.PrivateKey('d65788b9947b41625ffff946bc145187c6b85d1686e60becdf34567f17478730', 'testnet');
+           // 公開鍵2つ
+           var publicKey1 = privateKey1.publicKey;
+           var publicKey2 = privateKey2.publicKey;
+           // スクリプト生成
+           var P2SHScript = new bitcore.Script.buildMultisigOut([publicKey1, publicKey2], 1);
+           // スクリプトを出力
+           console.log(P2SHScript.toString());
+           // スクリプトのハッシュ値を生成
+           var P2SHFund = P2SHScript.toScriptHashOut();
+           // スクリプトを出力
+           console.log(P2SHFund.toString());
+           // トランザクション生成&署名
+           var tx = new bitcore.Transaction()
+             .from(utxo)
+             .to(P2SHFund.toAddress(), 9000)
+             .change(address)
+             .sign(privateKey);
+
+           console.log("tx",tx);
+           // ブロードキャストする。
+           broadcast(tx, function(id){
+                console.log("funded to",id);
+     　　　　　   // 2つ目のトランザクション生成&署名
+                var tx2 = new bitcore.Transaction()
+                    .from({txId:id, outputIndex:0, inputIndex:0, satoshis:9000, script:P2SHFund.toString()}, [publicKey1, publicKey2], 1)
+                    .to(address, 8000)
+                    .sign(privateKey2);
+
+                console.log("tx2",tx2.serialize());
+                // ブロードキャストする。
+                broadcast(tx2, function(id2){
+                    console.log("funded back to",id2);
+                    expect(id2).to.be.a("string");
+                    done()
+                });
+            });
+        });
+    })
  })
